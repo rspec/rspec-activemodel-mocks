b7868cda0d6f435b580b12054cb2f11faaf6de2b
Thomas Holmes <thomas@devminded.com>
2014-01-05 00:11:45 -0500

    Simplify sample app tests
    Switch to using an in memory sqlite3 db and loading up the
    schema in spec_helper. It's not super optimize but the schema
    is small and these tests are cheap.

--- templates/generate_stuff.rb
+++
@@ -1,2 +0,0 @@
-run('rake db:migrate')
-run('rake db:migrate RAILS_ENV=test')
--- templates/sample/spec/spec_helper.rb
+++ templates/sample/spec/spec_helper.rb
@@ -1,22 +1,12 @@
 $:<< File.join(File.dirname(__FILE__), '..')
 require 'active_record'
-require 'yaml'
 require 'rspec/active_model/mocks'
+require 'model/widget'
 
-require 'models/widget'
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 
-db_config = YAML::load(File.open('db/config.yml'))
-
-ActiveRecord::Base.establish_connection(db_config['test'])
+load 'db/schema.rb'
 
 RSpec.configure do |config|
-  config.around(:each) do |ex|
-    x = proc do
-      ex.call
-      raise ActiveRecord::Rollback
-    end
-    ActiveRecord::Base.transaction(&x)
-  end
-
   config.order = 'random'
 end
