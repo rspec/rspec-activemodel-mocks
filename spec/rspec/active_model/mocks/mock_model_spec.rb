f7ec7f4322dc8a613d7cd37bca1b22ab293b1d7e
Jason Barnabe <jason.barnabe@canadadrugs.com>
2017-01-17 15:12:40 -0600

    Fix use of deprecated 'stub'

--- spec/rspec/active_model/mocks/mock_model_spec.rb
+++ spec/rspec/active_model/mocks/mock_model_spec.rb
@@ -196,7 +196,7 @@ describe "mock_model(RealModel)" do
   describe "#has_attribute?" do
     context "with an ActiveRecord model" do
       before(:each) do
-        MockableModel.stub(:column_names).and_return(["column_a", "column_b"])
+        allow(MockableModel).to receive(:column_names).and_return(["column_a", "column_b"])
         @model = mock_model(MockableModel)
       end
 
